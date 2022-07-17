1beda359afceb0d81d5a39dca15060ceb9a882ad
tinku gupta <tinku.gupta4@gmail.com>
2022-07-17 23:45:32 +0530

    update css

--- src/App.js
+++ src/App.js
@@ -11,10 +11,8 @@ function App() {
   const [bmi, setBmi] = useState('')
   const [message, setMessage] = useState('')
 
-
-
   let calcBmi = (event) => {
-    //prevent submitting
+    //prevent submitting to the server
     event.preventDefault()
 
     if (weight === 0 || height === 0) {
@@ -50,25 +48,26 @@ function App() {
   //   }
   // }
 
-
   let reload = () => {
     window.location.reload()
   }
-
 
   return (
     <div className="app">
     <div className='container'>
       <h2 className='center'>BMI Calculator</h2>
       <form onSubmit={calcBmi}>
+
         <div>
           <label>Weight (lbs)</label>
           <input value={weight} onChange={(e) => setWeight(e.target.value)} />
         </div>
+
         <div>
           <label>Height (in)</label>
           <input value={height} onChange={(event) => setHeight(event.target.value)} />
         </div>
+
         <div>
           <button className='btn' type='submit'>Submit</button>
           <button className='btn btn-outline' onClick={reload} type='submit'>Reload</button>
